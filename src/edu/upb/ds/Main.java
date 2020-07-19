3b1fa7712672fc0bcbaea33086cb1dc0ba48ba65
Pedro Felipe Gomez Bonilla <pedro.gomez.2019@upb.edu.co>
2020-07-18 23:35:56 -0500

    Importing Scanner class

--- src/edu/upb/ds/Main.java
+++ src/edu/upb/ds/Main.java
@@ -1,8 +1,11 @@
 package edu.upb.ds;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello World!");
     }
 }
