c0e57d8dd1e5eac2e2ee3aceccef6f0d8023d347
Pedro Felipe Gomez Bonilla <pedro.gomez.2019@upb.edu.co>
2020-07-20 16:10:38 -0500

    Menu created

--- src/edu/upb/ds/Main.java
+++ src/edu/upb/ds/Main.java
@@ -6,6 +6,18 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hello World!");
+        boolean programExit=false;
+        while(!programExit){
+            System.out.println("\n#####################################");
+            System.out.println("\nWelcome to the Magic Square Generator");
+            System.out.println("\n#####################################");
+            System.out.println("\nPlease choose a option:");
+            System.out.println("\n1:Generate Magic Square");
+            System.out.println("\n2:Close Program");
+            System.out.println("\n:");
+            int num=sc.nextInt();
+
+        }
+
     }
 }
