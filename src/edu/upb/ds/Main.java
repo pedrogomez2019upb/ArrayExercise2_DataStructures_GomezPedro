59fa632fdc16231b069b5f283c3896f114f2ef06
Pedro Felipe Gomez Bonilla <pedro.gomez.2019@upb.edu.co>
2020-07-20 17:14:14 -0500

    Magic Box Working

--- src/edu/upb/ds/Main.java
+++ src/edu/upb/ds/Main.java
@@ -8,6 +8,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         boolean programExit=false;
         while(!programExit){
+            int option=0;
             System.out.println("\n#####################################");
             System.out.println("\nWelcome to the Magic Square Generator");
             System.out.println("\n#####################################");
@@ -15,9 +16,40 @@ public class Main {
             System.out.println("\n1:Generate Magic Square");
             System.out.println("\n2:Close Program");
             System.out.println("\n:");
-            int num=sc.nextInt();
+            option=sc.nextInt();
+            if(option ==1){
+                System.out.println("\n############################");
+                System.out.println("\nGenerate Magic Square");
+                System.out.println("\n############################");
+                System.out.println("\nPlease provide the square's size:");
+                int sizeSquare=sc.nextInt();
+                int[][] generateSquare = new int[sizeSquare][sizeSquare];
+                int x=sizeSquare-1;
+                int y=sizeSquare/2;
+                generateSquare[x][y]=1;
+                for(int i=2;i<=sizeSquare*sizeSquare;i++){
+                    if(generateSquare[(x+1)%sizeSquare][(y+1)%sizeSquare]==0){
+                        x=(x+1)%sizeSquare;
+                        y=(y+1)%sizeSquare;
+                    }
+                    else{
+                        x=(x-1+sizeSquare)%sizeSquare;
+                    }
+                    generateSquare[x][y]=i;
+                }
+                for(int i=0;i<sizeSquare;i++){
+                    for(int q=0;q<sizeSquare;q++){
+                        if (generateSquare[i][q]<10) System.out.print(" ");
+                        if (generateSquare[i][q]<100) System.out.print(" ");
+                        System.out.print(generateSquare[i][q]+" ");
+                    }
+                    System.out.println();
+                }
+            }
+                else if (option==2){
+                programExit=true;
+                }
 
+            }
         }
-
-    }
 }
